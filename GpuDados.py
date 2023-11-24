4f827b181f47025a5e471d3c32bb7602394ba38b
BryanSilva117 <bryan.liaris@sptech.school>
2023-11-24 01:48:04 -0300

    Slack e Log Adicionados

--- GpuDados.py
+++ GpuDados.py
@@ -2,6 +2,16 @@ import mysql.connector
 import GPUtil
 from datetime import datetime
 import time
+import logging
+import socket
+import requests
+
+webhook_url = "https://hooks.slack.com/services/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"
+
+
+
+logging.basicConfig(filename='log_performee_gpu.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 
 conexao = mysql.connector.connect(
     host='localhost',
@@ -11,6 +21,13 @@ conexao = mysql.connector.connect(
 )
 
 tentativas = 6
+vezestentada = 1
+
+
+hostname = socket.gethostname()
+
+
+ipUser = socket.gethostbyname(hostname)
 
 cursor = conexao.cursor()
 
@@ -30,12 +47,21 @@ Digite o IP do Servidor: """)
         print('\033[1;31mServidor não encontrado!\033[m')
         tentativas -= 1
         if tentativas == 0:
+            logging.info(
+                f' Usuário do IP {ipUser}, hostName: {hostname}. Esgotou o número máximo de tentativas para acessar o servidor! e o Python foi encerrado')
             print('Acabou suas tentativas! Volte mais tarde')
+
+
         else:
             print(f'Você tem \033[1;31m{tentativas}\033[m tentativas!'.format(tentativas))
+            logging.info(f' Usuário do IP {ipUser}, hostName: {hostname}. Tentou acessar um servidor inexistente pela {vezestentada} vez!')
+            vezestentada += 1
 
     else:
         print("\033[1;32mServidor Encontrado!\033[m")
+        logging.info(
+            f' Usuário do IP {ipUser}, hostName: {hostname}. Acessou o Servidor de IP: {ipServidor} com Sucesso!')
+
         while True:
             buscarEmp = f'select fkEmpresa from servidor where ipServidor = "{ipServidor}"'
             cursor.execute(buscarEmp)
@@ -76,11 +102,21 @@ Digite o IP do Servidor: """)
                                  f'"{ipServidor}")')
                     cursor.execute(insertGpu)
                     print("\033[1;32mGPU Cadastrada Com Sucesso!\033[m")
+                    logging.info(
+                        f' Usuário do IP {ipUser}, hostName: {hostname}. Cadastrou a GPU do servidor de IP: {ipServidor} com sucesso!')
+
                 else:
                     print("\033[1;33mGPU já cadastrada nesse servidor!\033[m")
+                    logging.info(
+                        f' Usuário do IP {ipUser}, hostName: {hostname}. Tentou cadastrar a GPU do servidor de IP: {ipServidor}, mas já existe GPU cadastrada')
+
 
 
             elif opcao == 2:
+
+                logging.info(
+                    f' Usuário do IP {ipUser}, hostName: {hostname}. iniciou o processo inserção de dados da GPU do servidor de IP: {ipServidor}')
+
                 while True:
                     dataHoraAtual = datetime.now()
                     pegaridComponente = f'select idComponente from Componente where tipo = "GPU" and fkServidor = "{ipServidor}"'
@@ -95,6 +131,109 @@ Digite o IP do Servidor: """)
                                         f'{fkComponente})')
                     cursor.execute(insertLeituraGpu) # executa o script do banco
                     conexao.commit()  # edita o banco de dados
+
+                    mediaGpu = f'SELECT ROUND(AVG(emUso), 2) AS media_ultimas_10_leituras FROM (SELECT emUso FROM leitura AS l JOIN componente AS c ON c.idComponente = l.fkComponente WHERE c.tipo = "GPU" AND l.fkServidor = "{ipServidor}" ORDER BY l.idLeitura DESC LIMIT 10 ) AS ultimas_leituras'
+                    cursor.execute(mediaGpu)
+                    gpuUso = cursor.fetchone()
+                    mediaUsoGpu = gpuUso[0]
+
+                    media_temperatura_gpu = f'SELECT ROUND(AVG(temperatura), 2) AS media_ultimas_10_leituras FROM (SELECT temperatura FROM leitura AS l JOIN componente AS c ON c.idComponente = l.fkComponente WHERE c.tipo = "GPU" AND l.fkServidor = "{ipServidor}" ORDER BY l.idLeitura DESC LIMIT 10) AS ultimas_leituras'
+                    cursor.execute(media_temperatura_gpu)
+                    temperaturaGpu = cursor.fetchone()[0]
+
+                    leitura = f'SELECT idLeitura FROM leitura AS l JOIN componente AS c ON c.idComponente = l.fkComponente WHERE c.tipo = "GPU" AND l.fkServidor = {ipServidor} ORDER BY l.idLeitura DESC LIMIT 1'
+                    cursor.execute(leitura)
+                    leituraFk = cursor.fetchone()
+                    fkLeitura = leituraFk[0]
+                    #
+                    if mediaUsoGpu >= 85:
+                        descricao = f"Alerta de Risco. Servidor {ipServidor}: A utilização da GPU esteve constantemente acima de 85%, nas últimas 10 verificação. Pode ocorrer travamentos! Média de utilização: {mediaUsoGpu}%"
+
+                        tipo = "Em risco"
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao}
+                        response = requests.post(webhook_url, json=alerta)
+
+                        logging.critical(
+                            f' Usuário do IP {ipUser}, hostName: {hostname}. Teve {descricao}')
+
+                    elif mediaUsoGpu >= 66 and mediaUsoGpu <= 84:
+                        descricao = f"Alerta de Cuidado. Servidor {ipServidor}: A utilização da GPU esteve constantemente entre 66% a 84%, nas últimas 10 verificação. Pode ocorrer lentidão! Média de utilização: {mediaUsoGpu}%"
+
+                        tipo = "Cuidado"
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao}
+                        response = requests.post(webhook_url, json=alerta)
+
+                        logging.warning(
+                            f' Usuário do IP {ipUser}, hostName: {hostname}. Teve {descricao}')
+                    else:
+                        descricao = f"Alerta Estável. Servidor {ipServidor}: A utilização da GPU está abaixo 66%, nas últimas 10 verificação. A utilização! Média de utilização: {mediaUsoGpu}%"
+
+                        tipo = 'Estável'
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao}
+                        response = requests.post(webhook_url, json=alerta)
+
+
+                    if temperaturaGpu > 39:
+                        descricao2 = f"Alerta de Risco. Servidor {ipServidor}: A temperatura da GPU está acima de 39°C, nas últimas 10 verificação. Risco de Super Aquecimento! Média de temperatura: {temperaturaGpu}°C"
+
+                        tipo = "Em risco"
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao2}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao2}
+                        response = requests.post(webhook_url, json=alerta)
+
+                        logging.critical(
+                            f' Usuário do IP {ipUser}, hostName: {hostname}. Teve {descricao}')
+                    elif temperaturaGpu >= 35 and temperaturaGpu <= 39:
+                        descricao2 = f"Alerta de Cuidado. Servidor {ipServidor}: A temperatura da GPU está entre 35°C a 39°C, nas últimas 10 verificação. Pode ocorrer aquecimento! Média de temperatura: {temperaturaGpu}°C"
+
+                        tipo = "Cuidado"
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao2}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao2}
+                        response = requests.post(webhook_url, json=alerta)
+
+                        logging.critical(
+                            f' Usuário do IP {ipUser}, hostName: {hostname}. Teve {descricao}')
+                    else:
+                        descricao2 = f"Alerta Estável. Servidor {ipServidor}: A temperatura da GPU está abaixo de 35°C, nas últimas 10 verificação. Temperatura está OK! Média de temperatura: {temperaturaGpu}°C"
+
+                        tipo = "Estável"
+
+                        insertAlerta = (
+                            f'insert into Alerta(dataAlerta, tipo, descricao, fkEmpresa, fkDataCenter, fkServidor, fkComponente, fkLeitura) values ("{dataHoraAtual}", "{tipo}", "{descricao2}", {fkEmpresa}, {fkDataCenter}, "{ipServidor}", {fkComponente}, {fkLeitura})')
+                        cursor.execute(insertAlerta)  # executa o script do banco
+                        conexao.commit()
+
+                        alerta = {"text": descricao2}
+                        response = requests.post(webhook_url, json=alerta)
+
                     time.sleep(15)
 
             elif opcao == 3:
